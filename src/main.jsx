09b4911d29a3a7e4d56422f332901ec7094674f9
bsrosa09 <breno0904@hotmail.com>
2025-10-20 14:01:11 -0300

    chore: initial commit

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import AmandaInvite from './AmandaInvite.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <AmandaInvite />
+  </React.StrictMode>,
+)
